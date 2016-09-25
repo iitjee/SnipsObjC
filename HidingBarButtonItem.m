446ea884cadee246b8ce579652e201fc26952d85
iitjee <iit.jee.help.me@gmail.com>
2016-09-26 00:06:26 +0530

    Create HidingBarButtonItem.m

---
+++ HidingBarButtonItem.m
@@ -0,0 +1,38 @@
+/*
+  1. A best example of Strong vs. Weak http://stackoverflow.com/questions/11013587/differences-between-strong-and-weak-in-objective-c
+  2. There's no direct way of hiding; you've to add and remove the object.  
+*/
+
+
+//1
+//Add a rightBarButtonItem in SB and Make a STRONG IBOutle
+
+//2
+    NSMutableArray *navigationBarButtons;
+
+//3
+    navigationBarButtons = [self.navigationItem.rightBarButtonItems mutableCopy];
+
+//4
+if(![navigationBarButtons containsObject:self.getFeedButton]) {
+        [navigationBarButtons addObject:self.getFeedButton];
+        [self.navigationItem setRightBarButtonItems:navigationBarButtons];
+    }
+
+//5 Removes object
+if(<#your-condition#> {
+    [navigationBarButtons removeObject:self.getFeedButton];
+    [self.navigationItem setRightBarButtonItems:navigationBarButtons];
+    }
+
+
+
+
+
+
+
+
+
+
+
+
