a0df292f7aaee258537ff21015b42f43e5ba899c
iitjee <iit.jee.help.me@gmail.com>
2016-09-26 15:34:50 +0530

    Update TapImageInsideCell.m

--- TableView/TapImageInsideCell.m
+++ TableView/TapImageInsideCell.m
@@ -1,14 +1,35 @@
-//1. In `cellForRowAtIndexPath` method
-cell.imageView.userInteractionEnabled = YES;
-cell.imageView.tag = indexPath.row; //assigns tag to each imageView
+/*
+    1. It's v.imp that property is strong. Check out what happens otherwise
+    2. 
+*/
 
-UITapGestureRecognizer *tapped = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(<#myTapFunction#>)];
+//1. In interface
+@property (strong, nonatomic) UIImageView *tappedImageView;
+
+
+//2. Initialize and configure the `tappedImageView` in viewDidLoad
+
+
+//3. In `cellForRowAtIndexPath` method
+cell.imageView.userInteractionEnabled = YES;
+cell.imageView.tag = indexPath.row; //assigns tag to each imageView, we don't need it. Just for educational purposes :)
+
+UITapGestureRecognizer *tapped = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(<#myTapFunction:#>)];
 tapped.numberOfTapsRequired = 1;
 [cell.imageView addGestureRecognizer:tapped];
 
-//2. Define myTapFunction
--(void)myTapFunction :(id) sender
-{
-    UITapGestureRecognizer *gesture = (UITapGestureRecognizer *) sender;
-    NSLog(@"Tag = %d", gesture.view.tag);
+//4. Define myTapFunction
+/* sender is v.important, it's the only way you know something about the item which user has tapped */
+-(void)myTapFunction :(UITapGestureRecognizer *) sender
+{   
+    NSLog(@"Tag = %d", sender.view.tag);
+    //Always copy the image, not the imageView itself
+    //_tappedImageView = (UIImageView *)sender.view  //fails horribly
+    _tappedImageView.image =  [(UIImageView *)sender.view image];
 }
+
+//(or) put id as argument type and get it like this.. no big differemce
+// -(void)myTapFunction :(id) sender
+// {   
+//     UITapGestureRecognizer *gesture = (UITapGestureRecognizer *) sender;
+// }
