a93079e9bc60cb5db6f3ca25a43afc4e79b87bd9
iitjee <iit.jee.help.me@gmail.com>
2016-09-28 10:46:42 +0530

    Update AlertPopup.m

--- AlertPopup.m
+++ AlertPopup.m
@@ -5,9 +5,12 @@
 UIAlertController* alertPopup = [UIAlertController alertControllerWithTitle:@"<#Title#>" message:@"<#Messages#>" preferredStyle:UIAlertControllerStyleAlert ];
 [self presentViewController:alertPopup animated:YES completion:nil];
 
-double delayInSeconds = <#1.0s#>;
+double delayInSeconds = <#1.0#>;
 dispatch_time_t popoutTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
 dispatch_after(popoutTime, dispatch_get_main_queue(), ^{
         // code executed on main queues after delay
         [alertPopup dismissViewControllerAnimated:YES completion:nil];
+        /* Do anything necessary on dismissal of alert popup */
     });
+
+//^this block method is much better than performSelector:FromObject:withDelay:
