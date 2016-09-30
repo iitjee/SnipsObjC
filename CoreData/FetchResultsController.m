5f1ea026df2bcfcfbd4d658f250877eb0e403686
iitjee <iit.jee.help.me@gmail.com>
2016-09-30 13:31:09 +0530

    Update FetchResultsController.m

--- CoreData/FetchResultsController.m
+++ CoreData/FetchResultsController.m
@@ -10,7 +10,7 @@ If all the three are used, It's referred as FULL PERSISTENT TRACKING
 Creating the Fetched Results Controller: You provide four parameters:
 - A fetch request. Atleast one sort descriptor to order the results + An optional Predicate
 - A managed object context. The controller uses this context to execute the fetch request.
-- Optionally, a key path on result objects that returns the section name. The controller uses the key path to split the results into sections (passing nil indicates that the controller should generate a single section).
+-*** Optionally, a key path on result objects that returns the section name. The controller uses the key path to split the results into sections (passing nil indicates that the controller should generate a single section).
 - Optionally, the name of the cache file the controller should use (passing nil prevents caching). Using a cache can avoid the overhead of computing the section and index information.
 
 Finally, Call performFetch:
@@ -27,23 +27,25 @@ Delegate Methods:
 - (void)controllerWillChangeContent:(NSFetchedResultsController *)controller; //usually you write [self.tableView beginUpdates]; in it
 
 - (void)controllerDidChangeContent:(NSFetchedResultsController *)controller;  //usually you write [self.tableView endUpdates]; in it
-
-- (void)controller:(NSFetchedResultsController *)controller 
-  didChangeSection:(id<NSFetchedResultsSectionInfo>)sectionInfo 
-           atIndex:(NSUInteger)sectionIndex 
-     forChangeType:(NSFetchedResultsChangeType)type;
-     
+   
 - (void)controller:(NSFetchedResultsController *)controller 
    didChangeObject:(id)anObject   //The object in controller’s fetched results that changed.
        atIndexPath:(NSIndexPath *)indexPath   //The index path of the changed object (this value is nil for insertions).
      forChangeType:(NSFetchedResultsChangeType)type   //type of change.
       newIndexPath:(NSIndexPath *)newIndexPath; //The destination path for the object for insertions or moves (this value is nil for a deletion).
       
+- (void)controller:(NSFetchedResultsController *)controller 
+  didChangeSection:(id<NSFetchedResultsSectionInfo>)sectionInfo 
+           atIndex:(NSUInteger)sectionIndex 
+     forChangeType:(NSFetchedResultsChangeType)type;
+  
+
 Notes: - beginUpdates and endUpdates methods of tableview can be used to refresh it, when there are changes in the MOC
        - types: NSFetchedResultsChangeInsert, NSFetchedResultsChangeDelete, NSFetchedResultsChangeMove, NSFetchedResultsChangeUpdate
 
 typical eg: 
 -------------
+//handles change in Entity Objects
 -(void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type newIndexPath:(NSIndexPath *)newIndexPath {
     
     UITableView *tableView = self.tableView;
@@ -71,7 +73,17 @@ typical eg:
     }
 
 }
-
+//Handles Change in Sections
+-(void) controller:(NSFetchedResultsController *)controller didChangeSection:(id<NSFetchedResultsSectionInfo>)sectionInfo atIndex:(NSUInteger)sectionIndex forChangeType:(NSFetchedResultsChangeType)type {
+    switch (type) {
+        case NSFetchedResultsChangeInsert:
+            [self.tableView insertSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+        case NSFetchedResultsChangeDelete:
+            [self.tableView deleteSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+    }
+}
 
 
 
