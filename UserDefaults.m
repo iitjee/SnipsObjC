c8afaf35eb69240e752c793205ea83bf8f710e88
iitjee <iit.jee.help.me@gmail.com>
2016-10-01 12:13:25 +0530

    Update UserDefaults.m

--- UserDefaults.m
+++ UserDefaults.m
@@ -20,3 +20,15 @@ NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
 NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
 NSArray *arrayOfImages = [userDefaults objectForKey:@"tableViewDataImage"];
 NSArray *arrayOfText = [userDefaults objectForKey:@"tableViewDataText"];
+
+
+/* Get all the Keys stored in NSUserDefaults */
+eg: You want to remove all the stored values for all the corresponding keys
+  - (void)resetDefaults {
+    NSUserDefaults * defs = [NSUserDefaults standardUserDefaults];
+    NSDictionary * dict = [defs dictionaryRepresentation];
+    for (id key in dict) {
+        [defs removeObjectForKey:key];
+    }
+    [defs synchronize];
+}
